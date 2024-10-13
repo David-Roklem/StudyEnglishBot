aca7a50b62da585bcaa742cf76ce284fe6a90ef7
David-Roklem <roklem@mail.ru>
2024-10-13 08:43:23 +0300

    feature_add_pydantic_settings

--- config.py
+++ config.py
@@ -6,11 +6,20 @@ from aiogram.client.default import DefaultBotProperties
 # from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv, find_dotenv
 # from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-load_dotenv(find_dotenv())
+# load_dotenv(find_dotenv())
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(case_sensitive=False)
+
+    bot_token: str
+
+
+settings = Settings()
 
 # storage = MemoryStorage()
-bot = Bot(os.getenv('TELEGRAM_TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(settings.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 # dp = Dispatcher(bot, storage=storage)
 dp = Dispatcher()
