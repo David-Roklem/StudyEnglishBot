6cf7c80fc800e5c7f72fcb7a0181d0850c5880b1
David-Roklem <roklem@mail.ru>
2024-10-16 09:19:52 +0300

    refact_rewrite_main.py_to_new_architecture

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import asyncio
 import logging
-# from aiogram import Dispatcher
-from handlers import handlers
-from config import dp, bot
-
-
-# handlers.register_all_handlers(dp)
-# from aiogram import types
-# @dp.message()
-# async def echo_msg(message: types.Message):
-#     await message.answer(text=message.text)
+from aiogram import Dispatcher, Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+# from handlers import handlers
+from config import settings
+from routers.commands import base_commands
 
 
 async def main():
+    bot = Bot(settings.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+
+    dp.include_router(base_commands.router)
+
     logging.basicConfig(level=logging.DEBUG)
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
