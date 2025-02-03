bda9f3eb8126cd96ae799267d0cc453618a09823
David-Roklem <roklem@mail.ru>
2025-02-03 13:26:46 +0300

    refact_base_handlers_cleansing

--- bot/handlers/base_handlers.py
+++ bot/handlers/base_handlers.py
@@ -1,20 +1,22 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
-from text_templates import base_text_templates
+from text_templates.inline_buttons import BUTTONS_START_MENU
+from text_templates import base_commands
+from keyboards.inline_keyboards.inline_kb import create_inline_kb
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def cmd_start(message: Message):
+async def cmd_start_testing_inline_kb(message: Message):
     """
-    Conversation's entry point
+    testing_inline_kb
     """
-    await message.answer(text=base_text_templates.start_cmd)
-    # <b>Выберите, пожалуйста, ваш уровень английского:</b>""", reply_markup=kbl, parse_mode="HTML")
+    keyboard = create_inline_kb(2, **BUTTONS_START_MENU)
+    await message.answer(text="Потестим Инлайн клаву", reply_markup=keyboard)
 
 
 @router.message(Command(commands='help'))
 async def process_help_command(message: Message):
-    await message.answer(text=base_text_templates.help_cmd)
+    await message.answer(text=base_commands.help_cmd)
