5ff4439128caace4c725d5ecd3d0cf8b52e717c2
David-Roklem <roklem@mail.ru>
2025-02-20 18:42:00 +0300

    fix_class_name

--- bot/bot_dialogs/books.py
+++ bot/bot_dialogs/books.py
@@ -4,11 +4,10 @@ from FSMstates import states
 from text_templates.inline_buttons import START_MENU_BUTTONS_TEXT
 from bot_dialogs.common import MAIN_MENU_BUTTON
 
-
 books_dialog = Dialog(
     Window(
         Const(START_MENU_BUTTONS_TEXT["books"]),
         MAIN_MENU_BUTTON,
-        state=states.Books.MAIN,
+        state=states.Book.MAIN,
     )
 )
