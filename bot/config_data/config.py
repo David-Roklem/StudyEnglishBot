6baf40146416afbd94d45628b2ec999d81218edb
David-Roklem <roklem@mail.ru>
2025-03-08 10:51:17 +0300

    add_alembic

--- bot/config_data/config.py
+++ bot/config_data/config.py
@@ -1,42 +1,35 @@
-import os
 from functools import lru_cache
-from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
 
-    model_config = SettingsConfigDict(case_sensitive=False)
-    bot_token: str
-
-    # Database config
-    DB_HOST: str
-    DB_NAME: str
-    DB_PORT: str
-    DB_USER: str
-    DB_PASS: str
-    IS_ECHO: str
-
-    DB_URL: str = Field('', validation_alias='DB_URL')
-
-    # other configs
+    model_config = SettingsConfigDict(case_sensitive=False, env_file=".env")
     bot_token: str
     admin_id: int
 
-    # class Config:
-    #     extra = 'ignore'
-    #     BASE_DIR = Path(__file__).resolve().parent.parent
-    #     env_file = os.path.join(BASE_DIR, '.env')
+    # Database config
+    # DB_HOST: str
+    # DB_NAME: str
+    # DB_PORT: str
+    # DB_USER: str
+    # DB_PASS: str
+    IS_ECHO: str
+
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+
+    DB_URL: str = Field('', validation_alias='DB_URL')
 
     def __init__(self, **data):
         super().__init__(**data)
         self.DB_URL = self.__construct_db_url()
 
     def __construct_db_url(self) -> str:
-        return (
-            f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-        )
+        return (f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+                f"localhost:5433/{self.POSTGRES_DB}")
 
 
 @lru_cache
